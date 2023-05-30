28421386f8c4d2317738dadb7d6a7ca513f283b1
shawn birring <sbirring4@my.bcit.ca>
2023-05-30 00:07:38 -0700

    Basic navbar and footer

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,148 @@
+"use client";
+import {
+  createStyles,
+  Header,
+  Menu,
+  Group,
+  Center,
+  Burger,
+  Container,
+  rem,
+  Drawer,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconChevronDown } from "@tabler/icons-react";
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    height: rem(56),
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    },
+  },
+
+  linkLabel: {
+    marginRight: rem(5),
+  },
+}));
+
+interface HeaderSearchProps {
+  links: {
+    link: string;
+    label: string;
+    links: { link: string; label: string }[];
+  }[];
+}
+
+export function Navbar({ links }: HeaderSearchProps) {
+  const [opened, { toggle }] = useDisclosure(false);
+  const { classes } = useStyles();
+
+  const items = links.map((link) => {
+    const menuItems = link.links?.map((item) => (
+      <Menu.Item key={item.link}>{item.label}</Menu.Item>
+    ));
+
+    if (menuItems) {
+      return (
+        <Menu
+          key={link.label}
+          trigger="hover"
+          transitionProps={{ exitDuration: 0 }}
+          withinPortal
+        >
+          <Menu.Target>
+            <a
+              href={link.link}
+              className={classes.link}
+              onClick={(event) => event.preventDefault()}
+            >
+              <Center>
+                <span className={classes.linkLabel}>{link.label}</span>
+                <IconChevronDown size="0.9rem" stroke={1.5} />
+              </Center>
+            </a>
+          </Menu.Target>
+          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+        </Menu>
+      );
+    }
+
+    return (
+      <a
+        key={link.label}
+        href={link.link}
+        className={classes.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </a>
+    );
+  });
+
+  const DrawerItems = items.map((item, index) => (
+    <Menu.Item key={index}>{item}</Menu.Item>
+  ));
+
+  return (
+    <Header height={56}>
+      <Container>
+        <div className={classes.inner}>
+          <Text>Hair by Cassie!</Text>
+          <Group spacing={10} className={classes.links}>
+            {items}
+          </Group>
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            className={classes.burger}
+            size="sm"
+          />
+        </div>
+      </Container>
+      <Drawer
+        opened={opened}
+        onClose={toggle}
+        padding="md"
+        size="xs"
+        position="right"
+      >
+        <Menu>{DrawerItems}</Menu>
+      </Drawer>
+    </Header>
+  );
+}
