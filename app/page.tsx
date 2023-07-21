8a6a8c65b437b3da03264d35c95a397255a95d62
shawnbirring <shawnbirring21@gmail.com>
2023-07-21 13:31:18 -0700

    new home page and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <p>Main page!</p>;
+  return (
+    <div className="relative">
+      <Image
+        src={"/main.jpg"}
+        width={1920}
+        height={1080}
+        alt="main"
+        className="w-full"
+      />
+      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
+          Welcome to HairStyle Studio
+        </h1>
+        <p className="text-lg md:text-xl mt-4">
+          We are passionate about creating stunning hairstyles that make you
+          look and feel fabulous.
+        </p>
+        <p className="text-lg md:text-xl mt-4">
+          Whether you need a haircut, color, or styling for a special occasion,
+          our experienced stylists are here to bring out the best in you.
+        </p>
+      </div>
+    </div>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,83 +1,41 @@
 "use client";
-import Image from "next/image";
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { Questrial } from "next/font/google";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import { IoIosMenu } from "react-icons/io";
+import { INavLink } from "@/models/INavLink";
 
-export default function Navbar() {
+const questrial = Questrial({ subsets: ["latin"], weight: "400" });
+
+export default function Navbar({ navLinks }: { navLinks: INavLink[] }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  const navLinks = [
-    { href: "/", text: "Home" },
-    { href: "/services", text: "Services" },
-    { href: "/gallery", text: "Gallery" },
-    { href: "/contact", text: "Contact" },
-    { href: "/appointment", text: "Book" },
-  ];
-
-  const toggleNav = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleScroll = () => {
-    const offset = window.scrollY;
-    if (offset > 80) {
-      setIsScrolled(true);
-    } else {
-      setIsScrolled(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
-    <nav className="bg-white border-gray-200 sticky top-0 z-50">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="/" className="flex items-center">
-          <Image
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8 mr-3"
-            alt="Flowbite Logo"
-            width={32}
-            height={32}
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap">
-            CVSS
-          </span>
-        </Link>
-        <button
-          onClick={toggleNav}
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-        >
-          <span className="sr-only">Open main menu</span>
-          <IoIosMenu className="w-6 h-6" aria-hidden="true" />
+    <nav
+      className={`${questrial.className} text-xl flex justify-between items-center text-black p-5`}
+    >
+      <span>
+        <strong>CVSS</strong>
+      </span>
+
+      <div className="md:hidden">
+        <button type="button" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
         </button>
-        <div
-          className={`${isOpen ? "" : "hidden"} w-full md:block md:w-auto`}
-          id="navbar-dropdown"
-        >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <Link
-                  href={link.href}
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                >
-                  {link.text}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
       </div>
-      {isScrolled && <hr className="border-gray-200 mx-8 border-t-1" />}
+
+      <div className={`${isOpen ? "block" : "hidden"} md:flex`}>
+        {navLinks.map((link) => (
+          <Link
+            href={link.url}
+            key={link.name}
+            className="p-2 mx-2 rounded hover:underline underline-offset-4"
+            onClick={() => setIsOpen(false)}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 }
