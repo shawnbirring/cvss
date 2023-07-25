c842e510f9b2ec4169504c1c8c26b54dba9f011b
shawnbirring <shawnbirring21@gmail.com>
2023-07-25 10:10:10 -0700

    removed use of index file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { Navbar, Footer } from "@/components";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import { INavLink } from "@/models/INavLink";
 
 export const metadata = {
